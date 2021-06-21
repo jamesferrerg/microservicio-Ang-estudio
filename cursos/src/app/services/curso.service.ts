8550a1383acb5a774e9ce4d98123cf4b501070dd
jamesferrerg <jamesjfgv@gmail.com>
2021-06-21 16:10:39 -0500

    Componente curso

--- cursos/src/app/services/curso.service.ts
+++ cursos/src/app/services/curso.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Curso } from '../models/curso';
 import { CommonService } from './common.service';
+import { HttpClient } from '@angular/common/http';
+import { BASE_ENDPOINT } from '../config/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursoService extends CommonService<Curso> {
 
-  protected baseEndPoint = 'http://localhost:8090/api/cursos';
+  protected baseEndPoint = BASE_ENDPOINT + '/cursos';
+
+  constructor(http: HttpClient) {
+    super(http);
+  }
 
 }
